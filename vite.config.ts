2c34f2fe097a901a5f21bdf64e4cb0adf80d540f
Ruben Oosterink <jb.ict.info@gmail.com>
2023-11-16 10:46:59 +0100

    Update github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/lablou/",
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
