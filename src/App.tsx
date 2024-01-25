e788234b8b629f014217b776e41146a8710cc413
Ruben Oosterink <jb.ict.info@gmail.com>
2024-01-25 16:17:56 +0100

    Voeg scroll to top toe

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./styles/styles.scss";
 import Header from "./components/Header/Header.tsx";
 import { Outlet } from "react-router";
 import Footer from "./components/Footer/Footer.tsx";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop.tsx";
 
 function App() {
   return (
     <>
       <Header />
+      <ScrollToTop />
       <Outlet />
       <Footer />
     </>
