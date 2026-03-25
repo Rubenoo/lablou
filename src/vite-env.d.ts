5daf0033471e10d6720c42866848724afadd724a
Rubenoo <jb.ict.info@gmail.com>
2026-03-25 14:44:21 +0100

    remove env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_BASIN_API_KEY: string;
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
