c38ac80d0c816df94c96cf12037024b961996530
Ruben O <jb.ict.info@gmail.com>
2026-07-22 15:33:43 +0200

    format vakantie

--- src/components/Banners/Vakantie.tsx
+++ src/components/Banners/Vakantie.tsx
@@ -4,8 +4,9 @@ import React from "react";
 const Vakantie: React.FunctionComponent = () => {
   return (
     <p className="vakantie-banner">
-      In verband met de zomervakantie zijn wij t/m 18 augustus gesloten voor flexwerken en vergaderen. Voor aanvragen na 18 augustus kun je mailen naar <a href="mailto:hallo@lablou.nl">hallo@lablou.nl</a>.
-      Fijne zomer!
+      In verband met de zomervakantie zijn wij t/m 18 augustus gesloten voor
+      flexwerken en vergaderen. Voor aanvragen na 18 augustus kun je mailen naar{" "}
+      <a href="mailto:hallo@lablou.nl">hallo@lablou.nl</a>. Fijne zomer!
     </p>
   );
 };
